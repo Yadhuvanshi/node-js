eb2650c07275373c8930256be0834fc3d47369b5
Yadhuvanshi <yadavshivam349@gmail.com>
2023-06-06 04:14:50 -0700

    ejstemlate-dynamic routing

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express=require('express');
 const path=require('path');
 const publicpath=path.join(__dirname,'public');
 const app=express();
+app.set('view engine','ejs');
 app.get('/',(_,resp)=>{resp.sendFile(`${publicpath}/index.html`)});
 app.get('/about',(_,resp)=>{resp.sendFile(`${publicpath}/about.html`)});
+app.get('/profile',(_,resp)=>{const user=
+    {
+        name:'anil sidhhu',
+        email:'unlink@gmail.com',
+        city:'noida'
+    }
+resp.render('profile',{user});});
 app.get('/help',(_,resp)=>{resp.sendFile(`${publicpath}/help.html`)});
 app.get('*',(_,resp)=>{resp.sendFile(`${publicpath}/helpcopy.html`)});
 app.listen(5000);
