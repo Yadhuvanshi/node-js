5bfb04a9d7debaa4a9e6c193b929c272a2d78d90
Yadhuvanshi <yadavshivam349@gmail.com>
2023-06-06 23:10:21 -0700

    applying group routing

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express=require('express');
 const app=express();
 const reqFilter=require('./middleware');
 //app.use(reqFilter);
+const route=express.Router();
+route.use(reqFilter);
 app.get('/',(req,resp)=>{
     resp.send("this is the home page")
 })
-app.get('/user',reqFilter,(req,resp)=>{
+app.get('/user',(req,resp)=>{
     resp.send("this is the user page")
 })
+route.get('/about',(req,resp)=>{
+    resp.send("this is the about page")
+})
+route.get('/help',(req,resp)=>{
+    resp.send("this is the help page")
+})
+app.use('/',route);
 app.listen(5000)
 
 
